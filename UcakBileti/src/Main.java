851bcf3c6d0f2f1fbf8cbfc3c0e541c37f6cde82
dozdemirz <ozdemirdilan@hotmail.com>
2023-09-13 18:48:31 +0300

    UcakBileti

--- UcakBileti/src/Main.java
+++ UcakBileti/src/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
         if (km > 0) {
             km = km;
-        }else {
+        } else {
             System.out.println("Hatalı giriş yaptınız.");
-            System. exit(0);
+            System.exit(0);
         }
 
         System.out.println("Yaşınızı giriniz:");
@@ -25,31 +25,31 @@ public class Main {
 
         if (yas > 0) {
             yas = yas;
-        }else {
+        } else {
             System.out.println("Hatalı giriş yaptınız.");
-            System. exit(0);
+            System.exit(0);
         }
 
-        System.out.println("Yolculuk tipinizi seçiniz 1/2? 1: Tek yön, 2: Gidiş dönüş");
+        System.out.println("Yolculuk tipinizi seçiniz 1/2?  \n 1: Tek yön \n 2: Gidiş dönüş");
         yolculuktipi = input.nextInt();
 
         if (yolculuktipi == 1 || yolculuktipi == 2) {
             yolculuktipi = yolculuktipi;
-        }else {
+        } else {
             System.out.println("Hatalı giriş yaptınız.");
-            System. exit(0);
+            System.exit(0);
         }
 
         if (yas > 0 && yas < 12) {
             yasindirimi = (km * kmfiyat) * 0.5;
             System.out.println("Yaş indiriminiz:" + yasindirimi);
             fiyat = (km * kmfiyat) - (yasindirimi);
-            System.out.println("İndirimli tutarınız:" + fiyat);
+            System.out.println("Yaş indirimli tutarınız:" + fiyat);
         } else if (yas >= 12 && yas <= 24) {
             yasindirimi = (km * kmfiyat) * 0.1;
             System.out.println("Yaş indiriminiz:" + yasindirimi);
             fiyat = (km * kmfiyat) - (yasindirimi);
-            System.out.println("İndirimli tutarınız:" + fiyat);
+            System.out.println("Yaş indirimli tutarınız:" + fiyat);
         } else if (yas > 24 && yas <= 65) {
             fiyat = km * kmfiyat;
             System.out.println("Yaş indiriminiz bulunmamaktadır.");
@@ -57,20 +57,20 @@ public class Main {
             yasindirimi = (km * kmfiyat) * 0.3;
             System.out.println("Yaş indiriminiz:" + yasindirimi);
             fiyat = (km * kmfiyat) - (yasindirimi);
-            System.out.println("İndirimli tutarınız:" + fiyat);
+            System.out.println("Yaş indirimli tutarınız:" + fiyat);
         }
 
 
         switch (yolculuktipi) {
             case 1:
-                System.out.println("İndiriminiz bulunmamaktadır");
+                System.out.println("Uçuş tipine göre indiriminiz bulunmamaktadır");
                 System.out.println("Toplam fiyatınız :" + fiyat);
                 break;
             case 2:
                 tipİndirimi = (fiyat) * 0.2;
                 System.out.println("Gidiş dönüş bilet indiriminiz: " + tipİndirimi);
                 fiyat1 = (fiyat - tipİndirimi) * 2;
-                System.out.println("Toplam fiyatınız: " + fiyat1);
+                System.out.println("2 Bilet toplam fiyatınız: " + fiyat1);
                 break;
             default:
                 System.out.println("Hatalı giriş yaptınız.");
