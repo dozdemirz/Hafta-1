116f9dd6fb96445916cea069d487564384393c7a
dozdemirz <ozdemirdilan@hotmail.com>
2023-09-13 16:51:26 +0300

    SinifiGecme

--- KullaniciGiris/src/Main.java
+++ KullaniciGiris/src/Main.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        String dogrusifre = "12345";
+        String yenisifre, secenek, girilensifre;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Lütfen şifrenizi giriniz: ");
+        girilensifre = scanner.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (girilensifre.equals(dogrusifre)) {
+            System.out.println("Giriş başarılı.");
+        } else {
+            System.out.println("Hatalı şifre girişi. Şifrenizi sıfırlamak ister misiniz? (Evet/Hayır): ");
+            secenek = scanner.nextLine();
+
+            if (secenek.equalsIgnoreCase("Evet")) {
+                System.out.print("Yeni şifrenizi giriniz: ");
+                yenisifre = scanner.nextLine();
+
+                if (yenisifre.equals(dogrusifre)) {
+                    System.out.println("Yeni şifre eski şifre ile aynı olamaz. Şifre sıfırlanmadı.");
+
+                } else {
+                    System.out.println("Şifre sıfırlandı ve güncellendi.");
+                }
+            }
         }
     }
 }
