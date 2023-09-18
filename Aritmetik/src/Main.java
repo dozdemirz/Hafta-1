fe17de0e27aa2baf27e008d7556bef57d9340b49
dozdemirz <ozdemirdilan@hotmail.com>
2023-09-18 05:26:03 +0300

    Aritmetik

--- Aritmetik/src/Main.java
+++ Aritmetik/src/Main.java
@@ -2,21 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("a sayısını girin: ");
-        double a = input.nextDouble();
+        double a = scanner.nextDouble();
 
         System.out.print("b sayısını girin: ");
-        double b = input.nextDouble();
+        double b = scanner.nextDouble();
 
         System.out.print("c sayısını girin: ");
-        double c = input.nextDouble();
+        double c = scanner.nextDouble();
 
-        double sonuc = a + b * c - b;
+        System.out.println("İşlem sırasını seçin:");
+        System.out.println("1. (a+b)*c-b");
+        System.out.println("2. a+(b*c)-b");
+        System.out.println("3. a+b*(c-b)");
+        int secim = scanner.nextInt();
 
-        System.out.println("İşlem sonucu: " + sonuc);
+        double sonuc = 0.0;
 
+        switch (secim) {
+            case 1:
+                sonuc = (a + b) * c - b;
+                break;
+            case 2:
+                sonuc = a + (b * c) - b;
+                break;
+            case 3:
+                sonuc = a + b * (c - b);
+                break;
+            default:
+                System.out.println("Geçersiz seçenek!");
+                System.exit(1);
+        }
 
+        System.out.println("Sonuç: " + sonuc);
+        scanner.close();
     }
 }
